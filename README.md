c1b7fae191f911eda2ebfba551d6cdf626a97b35
Egor Boyko <leferchian@gmail.com>
2022-11-28 01:16:36 +0300

    InitialCommit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/8ndty1t8kd65pmmy?svg=true)](https://ci.appveyor.com/project/Leferchian/javaautoles3)
---
+++ src/test/java/TestingTheForm.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class TestingTheForm {
+    private SelenideElement form;
+    private String validName = "Петр Аркадьевич";
+    private String validPhone = "+79106697097";
+
+
+    @Test
+    public void shouldSendForm() {
+        open("http://localhost:9999");
+        form = $("form");
+        form.$("[data-test-id=name] input").setValue(validName);
+        form.$("[data-test-id=phone] input").setValue(validPhone);
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+
+        $("[data-test-id = order-success]").shouldHave(exactText(expected));
+    }
+
+}
